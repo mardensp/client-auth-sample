f3bc0f0ef9dd46c33f3191c6e827045a1a604b19
marden.paiva <mardensp@hotmail.com>
2022-09-20 12:41:32 -0400

    Ajustado endpoint  do serviço do auth-sample

--- src/main/java/com/example/clientauthsample/dto/SampleDTO.java
+++ src/main/java/com/example/clientauthsample/dto/SampleDTO.java
@@ -1,34 +1,14 @@
 package com.example.clientauthsample.dto;
 
-import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class SampleDTO {
 
     private Boolean authenticated;
-    private String username;
-    private List<String> roles;
-
-    public Boolean isAuthenticated() {
-        return authenticated;
-    }
-
-    public void setAuthenticated(Boolean authenticated) {
-        this.authenticated = authenticated;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
+    private String id;
 }
